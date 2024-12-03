8b20e097cd81f12eff5bda3a72e47c126633d1bb
Luca Frangiamore <Luca.Frangiamore@worlwolterskluwer.com>
2024-12-03 17:41:09 +0100

    feat:rust

--- src/day3/mull_it_over.rs
+++ src/day3/mull_it_over.rs
@@ -1,33 +1,40 @@
 use std::{collections::HashMap, fs};
-struct to_find {
-    to_find:String,
-    map:HashMap<String,i32>,
-}
+use regex::Regex;
+
 fn read_content(path:String)->String{
     return fs::read_to_string(path).expect("Unable to read file");
 }
-//check string using pattern, return None if pattern is founded, if some return index of first
-//different char
-fn check_range(i_start:usize,patter:&to_find,str:&String)->Option<usize>{
-    let mut checked=patter.to_find.len()-1;
-    for i in (i_start..i_start+patter.to_find.len()).rev(){
-        if str.chars().nth(i)!=patter.to_find.chars().nth(checked){
-            return Some(i);
-        }
-        if checked==0{
-            return None;
-        }
-        checked-=1;
 
+fn find (str:String)->Result<Vec<(i32,i32)>,String>{
+    //input = ( number,number) 
+    fn get_parm(str:String)->Result<(i32,i32),String>{
+        let value:String=str[1..str.len()-2].to_string();
+        let mut split = value.split(",");
+        if split.clone().count() != 2{
+            return Err("Invalid input".to_string());
+        }
+        let a=split.next().unwrap().parse::<i32>().unwrap();
+        let b=split.next().unwrap().parse::<i32>().unwrap();
+        return Ok((a,b));
     }
-    return None;
-}
-
-fn find (patter:to_find,str:String)->Vec<i32>{
-    let i_init:usize=0;
-    let i_end:usize=patter.to_find.len();
-    Vec::new()
-    
+    let reg=Regex::new(r"mul\([0-9]+,[0-9]+\)");
+    if reg.is_err(){
+        return Err(reg.unwrap_err().to_string());
+    }
+    let reg=reg.unwrap();
+    let mut res:Vec<(i32,i32)>=Vec::new();
+    let matchs=reg.split(&str);
+    let mut numbers:(i32,i32);
+    for m in matchs{
+        println!("{}",m);
+        match (get_parm(m.to_string())){
+            Err(e) => return Err(e),
+            Ok(m) => {
+                res.push(m);
+            }
+        }
+    }
+    return Ok(res);
 }
 
 mod test{
@@ -36,5 +43,12 @@ mod test{
     fn test_read_content(){
         let a=read_content("src/day3/input".to_string());
         assert_ne!(a,"");
+    }
+    #[test]
+    fn test_match(){
+        let a=read_content("src/day3/input".to_string());
+        let r=find(a);
+        println!("{:?}",r);
+        assert_eq!(r.is_ok(),true);
     }
 }
