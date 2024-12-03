f4ac7685158ed98bd50134382bd26217f723f36d
Luca Frangiamore <Luca.Frangiamore@worlwolterskluwer.com>
2024-12-03 15:51:02 +0100

    feat:pattern maching struttura

--- src/day3/mull_it_over.rs
+++ src/day3/mull_it_over.rs
@@ -1,6 +1,19 @@
-use std::fs;
+use std::{collections::HashMap, fs};
+struct to_find {
+    to_find:String,
+    map:HashMap<String,i32>,
+}
 fn read_content(path:String)->String{
     return fs::read_to_string(path).expect("Unable to read file");
+}
+
+fn find (patter:to_find,str:String)->Vec<i32>{
+    fn check_range(i_end:usize)->Option<usize>{
+        
+    }
+    let i_init:usize=0;
+    let i_end:usize=a.to_find.len();
+    
 }
 
 mod test{
