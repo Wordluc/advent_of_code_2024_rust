03501be1abd49e55c8a35456060a5eedef31c37c
Luca Frangiamore <Luca.Frangiamore@worlwolterskluwer.com>
2024-12-03 16:07:50 +0100

    feat:pattern maching struttura

--- src/day3/mull_it_over.rs
+++ src/day3/mull_it_over.rs
@@ -6,13 +6,27 @@ struct to_find {
 fn read_content(path:String)->String{
     return fs::read_to_string(path).expect("Unable to read file");
 }
+//check string using pattern, return None if pattern is founded, if some return index of first
+//different char
+fn check_range(i_start:usize,patter:&to_find,str:&String)->Option<usize>{
+    let mut checked=patter.to_find.len()-1;
+    for i in (i_start..i_start+patter.to_find.len()).rev(){
+        if str.chars().nth(i)!=patter.to_find.chars().nth(checked){
+            return Some(i);
+        }
+        if checked==0{
+            return None;
+        }
+        checked-=1;
+
+    }
+    return None;
+}
 
 fn find (patter:to_find,str:String)->Vec<i32>{
-    fn check_range(i_end:usize)->Option<usize>{
-        
-    }
     let i_init:usize=0;
-    let i_end:usize=a.to_find.len();
+    let i_end:usize=patter.to_find.len();
+    Vec::new()
     
 }
 
